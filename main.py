677da6d04804aa97f218fe632e65246b128dcdd7
AntonioBerna <60107274+AntonioBerna@users.noreply.github.com>
2020-05-25 12:56:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import turtle as t
 
+def controllo_input(iterazione):
+    if iterazione == "":
+        iterazione = 4
+    else:
+        iterazione = int(iterazione)
+
+    return iterazione
+
+def move():
+    t.penup()
+    t.backward(lunghezza / 2)  # spostamento penna
+    t.pendown()
+
+
+def fiocco_di_neve_koch(lunghezza, iterazione):
+    for i in range(3):
+        koch(lunghezza, iterazione)
+        t.right(120)
+
+
 def koch(lunghezza, iterazione):
     if iterazione == 0:
         t.forward(lunghezza)
@@ -13,22 +33,15 @@ def koch(lunghezza, iterazione):
         t.left(60)
         koch(lunghezza, iterazione - 1)
 
+
 bob = t.Turtle()
 
 t.speed(0)  # velocità max
 lunghezza = 300
 iterazione = input("Inserisci il numero di iterazioni della curva di Koch (default -> 4): ")
-if iterazione == "":
-    iterazione = 4
-else:
-    iterazione = int(iterazione)
+iterazione = controllo_input(iterazione)
 
-t.penup()
-t.backward(lunghezza/2)  # spostamento penna
-t.pendown()
-
-for i in range(3):
-    koch(lunghezza, iterazione)
-    t.right(120)
+move()
+fiocco_di_neve_koch(lunghezza, iterazione)
 
 t.mainloop()
